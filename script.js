4f8431f1a994a9d82ef28444f15b6fc50274c578
valdir244 <valdeilton.neto@escola.pr.gov.br>
2025-09-05 17:15:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click" , mostraPalavrasChave);
+function mostraPalavrasChave(){
+    alert("fui clicado!");
+}
